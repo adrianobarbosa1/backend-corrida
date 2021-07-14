26d2b822a059a0107242e8a6eb4de1dc2f8aa1df
adriano barbosa <adriano.barbosa100@gmail.com>
2021-07-14 16:59:58 -0300

    title e version como deve ser

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 
 router.use('/api/v1', require('./api/v1/'))
-router.get('/', (req, res, next) => res.send({ ok: true }))
+router.get('/', (req, res, next) => res.status(200).send({
+    title: "API CORRIDA",
+    version: "0.0.1"
+}))
 
-router.use(function(err, req, res, next){
-    if(err.name === 'ValidationError'){
+router.use(function (err, req, res, next) {
+    if (err.name === 'ValidationError') {
         return res.status(422).json({
-            errors: Object.keys(err.errors).reduce(function(errors, key){
-                errors[key] = err. errors[key.message]
+            errors: Object.keys(err.errors).reduce(function (errors, key) {
+                errors[key] = err.errors[key.message]
                 return errors
             }, {})
         })
