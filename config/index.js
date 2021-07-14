1fd866fd8f212e8b02a37622997a3b2249d15d74
adriano barbosa <adriano.barbosa100@gmail.com>
2021-07-14 16:22:23 -0300

    secret

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : "WREFFEFD8FEWF231FEW64654FEWf354doihfeonkf564F",
-    api: process.env.NODE_ENV === 'production' ? "http://api.anapolis.go.gov.br" : 'http://localhost:3000',
+    secret: "WREFFEFD8FEWF231FEW64654FEWf354doihfeonkf564F",
+    api: process.env.NODE_ENV === 'production' ? "http://api.anapolis.go.gov.br/apiCorrida" : 'http://localhost:3000',
    // loja: process.env.NODE_ENV === 'production' ? "firebase" : 'http://localhost:8000',
 }
