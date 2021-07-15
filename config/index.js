6c5ff7b76b79d7c05d9f041329c4ee6c21f39a75
adriano barbosa <adriano.barbosa100@gmail.com>
2021-07-15 15:19:50 -0300

    admin validação para super-adm

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: "WREFFEFD8FEWF231FEW64654FEWf354doihfeonkf564F",
-    api: process.env.NODE_ENV === 'production' ? "http://api.anapolis.go.gov.br/apiCorrida" : 'http://localhost:3000',
+    api: process.env.NODE_ENV === 'production' ? "http://api.anapolis.go.gov.br/apiCorrida" : 'http://localhost:3006',
    // loja: process.env.NODE_ENV === 'production' ? "firebase" : 'http://localhost:8000',
 }
