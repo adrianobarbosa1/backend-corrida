b4ba3a46ccb2c2bdfd9f52ac7e22cd5b1ad1e7e2
drk <adriano.barbosa100@gmail.com>
2022-04-12 09:27:48 -0300

    route google ok

--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -2,55 +2,53 @@ const Joi = require("joi");
 const { password, objectId } = require('./custom.validation');
 
 const createUser = {
-    body: Joi.object().keys({
-        cpf: Joi.string().length(14).optional(),
-        password: Joi.string(),
-        name: Joi.string().required(),
-        email: Joi.string().email().required(),
-        role: Joi.string().required().valid('user', 'admin'),
-    })
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string(),
+    name: Joi.string().required(),
+    role: Joi.string().required().valid('user', 'admin'),
+  })
 };
 
 const getUser = {
-    params: Joi.object().keys({
-        userId: Joi.string().custom(objectId),
-    }),
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
 };
 
 const getUsers = {
-    query: Joi.object().keys({
-        name: Joi.string(),
-        role: Joi.string(),
-        sortBy: Joi.string(),
-        limit: Joi.number().integer(),
-        page: Joi.number().integer(),
-    }),
+  query: Joi.object().keys({
+    name: Joi.string(),
+    role: Joi.string(),
+    sortBy: Joi.string(),
+    limit: Joi.number().integer(),
+    page: Joi.number().integer(),
+  }),
 };
 
 const updateUser = {
-    params: Joi.object().keys({
-        userId: Joi.required().custom(objectId),
-    }),
-    body: Joi.object()
-        .keys({
-            cpf: Joi.string().length(14).optional(),
-            password: Joi.string().custom(password),
-            name: Joi.string(),
-            email: Joi.string().email(),
-        })
-        .min(1),
+  params: Joi.object().keys({
+    userId: Joi.required().custom(objectId),
+  }),
+  body: Joi.object()
+    .keys({
+      email: Joi.string().email(),
+      password: Joi.string().custom(password),
+      name: Joi.string(),
+    })
+    .min(1),
 };
 
 const deleteUser = {
-    params: Joi.object().keys({
-        userId: Joi.string().custom(objectId),
-    }),
+  params: Joi.object().keys({
+    userId: Joi.string().custom(objectId),
+  }),
 };
 
 export default {
-    createUser,
-    getUser,
-    getUsers,
-    updateUser,
-    deleteUser
+  createUser,
+  getUser,
+  getUsers,
+  updateUser,
+  deleteUser
 };
