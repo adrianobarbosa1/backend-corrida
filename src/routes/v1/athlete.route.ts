9184c60e86b2be32018aab58c01b079cb282dfd2
adriano.barbosa100 <adriano.barbosa100@gmail.com>
2022-05-25 15:59:00 -0300

    att

--- src/routes/v1/athlete.route.ts
+++ src/routes/v1/athlete.route.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import multer from 'multer';
+
 import { auth } from '../../middlewares/auth';
 import validate from '../../middlewares/validate';
 import { athleteValidation } from '../../validations';
 import { athleteController } from '../../controllers';
+import { upload } from '../../middlewares/multer';
 
 const router = express.Router();
 
@@ -11,5 +14,14 @@ router
   .post(auth(), validate(athleteValidation.createAthlete), athleteController.createAthlete)
   .get(auth(), validate(athleteValidation.showAthlete), athleteController.showAthlete)
   .patch(auth(), validate(athleteValidation.updateAthlete), athleteController.updateAthlete);
+
+router
+  .route('/img')
+  .patch(auth(), validate(athleteValidation.uploadFoto), multer(upload).single("avatar"), athleteController.uploadFoto)
+  .get(auth(), validate(athleteValidation.uploadFoto), athleteController.showUploadFoto);
+
+router
+  .route('/:idEvento')
+  .patch(auth(), validate(athleteValidation.registerEvent), multer(upload).single("avatar"), athleteController.uploadFoto)
 
 export default router;
